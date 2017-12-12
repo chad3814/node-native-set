1ba432a52fe77ae29219abbba723c8d7f629d929
Andrea del Rio <adelrio@gmail.com>
2017-12-12 10:58:28 -0800

    add packages

---
+++ test/set.test.js
@@ -0,0 +1,5 @@
+// var test = require('tape');
+// var request = require('request');
+// var queue = require('queue-async');
+// var zlib = require('zlib');
+// var forOf = require('es6-iterator/for-of');
