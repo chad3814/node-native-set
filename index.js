4b65b79752a17e6228fd697ab01b67aa5476aaac
Andrea del Rio <adelrio@gmail.com>
2017-12-12 15:46:13 -0800

    functional tests sorta

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
 'use strict';
-var assert = require("assert");
 var NativeSet = require('./build/Release/native').NodeSet;
-var Set = require('./src/set.cpp')
-
-function validate(data) {
-
-
-}
-
-
 
 module.exports = NativeSet;
--- test/set.test.js
+++ test/set.test.js
@@ -3,24 +3,22 @@ var Set = require('../index.js');
 
 test('test set', function (t) {
     var set = new Set();
-    t.throws(function() {set.add('raz').add('dwa').add({})});
-    t.throws(function() {set.size}); // 3
-    t.throws(function() {set.has('raz')}); // true
-    t.throws(function() {set.has('foo')}); // false
-    t.throws(function() {set.add('foo')}); // set
-    t.throws(function() {set.size}); // 4
-    t.throws(function() {set.has('dwa')}); // true
-    t.throws(function() {set.delete('dwa')}); //true
-    t.throws(function() {set.size}); // 3
-    set.forEach(function (value) {
-      // 'raz', {}, 'foo' iterated
-    });
-})
-
-test('test iterator', function (t) {
-  var iterator = set.values()
-  t.throws(function() {iterator.next()}); // { done: false, value: 'raz' }
-  t.throws(function() {iterator.next()}); // { done: false, value: {} }
-  t.throws(function() {iterator.next()}); // { done: false, value: 'foo' }
-  t.throws(function() {iterator.next()}); // { done: true, value: undefined }
+    var iterator = set.values()
+    t.ok(function() {set.add('raz').add('dwa').add({})});
+    t.ok(function() {set.size}); // 3
+    t.ok(function() {set.has('raz')}); // true
+    t.ok(function() {set.has('foo')}); // false
+    t.ok(function() {set.add('foo')}); // set
+    t.ok(function() {set.size}); // 4
+    t.ok(function() {set.has('dwa')}); // true
+    t.ok(function() {set.delete('dwa')}); //true
+    t.ok(function() {set.size}); // 3
+    t.ok(function() {iterator.next()}); // { done: false, value: 'raz' }
+    t.ok(function() {iterator.next()}); // { done: false, value: {} }
+    t.ok(function() {iterator.next()}); // { done: false, value: 'foo' }
+    t.ok(function() {iterator.next()}); // { done: true, value: undefined }
+    // set.forEach(function (value) {
+    //   // 'raz', {}, 'foo' iterated
+    // });
+    t.end();
 })
