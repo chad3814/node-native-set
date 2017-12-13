21b83d39c4a41e5ca4841cbf6a08f6ceac1c990e
Andrea del Rio <adelrio@gmail.com>
2017-12-12 16:43:53 -0800

    more function tests

--- test/set.test.js
+++ test/set.test.js
@@ -1,24 +1,35 @@
 var test = require('tape');
-var Set = require('../index.js');
+var NodeSet = require('../index.js');
 
 test('test set', function (t) {
-    var set = new Set();
-    var iterator = set.values()
-    t.ok(function() {set.add('raz').add('dwa').add({})});
-    t.ok(function() {set.size}); // 3
-    t.ok(function() {set.has('raz')}); // true
-    t.ok(function() {set.has('foo')}); // false
-    t.ok(function() {set.add('foo')}); // set
-    t.ok(function() {set.size}); // 4
-    t.ok(function() {set.has('dwa')}); // true
-    t.ok(function() {set.delete('dwa')}); //true
-    t.ok(function() {set.size}); // 3
-    t.ok(function() {iterator.next()}); // { done: false, value: 'raz' }
-    t.ok(function() {iterator.next()}); // { done: false, value: {} }
-    t.ok(function() {iterator.next()}); // { done: false, value: 'foo' }
-    t.ok(function() {iterator.next()}); // { done: true, value: undefined }
-    // set.forEach(function (value) {
-    //   // 'raz', {}, 'foo' iterated
-    // });
+    var set = new NodeSet();
+    t.equal(set.size, 0, 'set size is 0');
+
+    set.add('raz').add('dwa').add({});
+    t.equal(set.size, 3, 'set size is 3');
+
+    set.add('foo');
+
+    t.ok(set.has('raz'), 'set has raz');
+
+    set.delete('dwa')
+    t.notOk(set.has('dwa'), 'dwa deleted')
+    t.notOk(set.has('andrea'), 'andrea was not here')
+
+    var iterator = set.values();
+    t.ok(iterator.next());
+    // t.ok(function() {iterator.next()}); // { done: false, value: 'raz' }
+    // t.ok(function() {iterator.next()}); // { done: false, value: {} }
+    // t.ok(function() {iterator.next()}); // { done: false, value: 'foo' }
+    // t.ok(function() {iterator.next()}); // { done: true, value: undefined }
+    // test array
+    // test iterator
+
     t.end();
+})
+
+test('test iterator', function(t) {
+  var set = new Set();
+  var iterator = set.values();
+  t.end();
 })
