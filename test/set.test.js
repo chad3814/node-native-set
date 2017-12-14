12cd2cf7f243384b841f67ef67da6ff9a585cfd8
Anjana Vakil <anjana.vakil@mapbox.com>
2017-12-14 11:47:16 -0800

    Clean up set.test.js

--- test/set.test.js
+++ test/set.test.js
@@ -90,7 +90,7 @@ var test = require('tape');
    test(`test ${setType} iteration with .forEach()`, (assert) => {
      var mySet = new Set();
      mySet.add(0);
-     mySet.add(1);  
+     mySet.add(1);
 
      let values = [];
      assert.doesNotThrow(() => {
@@ -131,59 +131,3 @@ var test = require('tape');
    });
 
 })
-// ```
-// > var s;
-// undefined
-// > s = new Set([1,2,3]);
-// Set { 1, 2, 3 }
-// > s = new Set({1:2, 3:4})
-// TypeError: (var)[Symbol.iterator] is not a function
-//     at new Set (native)
-//     at repl:1:5
-//     at sigintHandlersWrap (vm.js:22:35)
-//     at sigintHandlersWrap (vm.js:73:12)
-//     at ContextifyScript.Script.runInThisContext (vm.js:21:12)
-//     at REPLServer.defaultEval (repl.js:340:29)
-//     at bound (domain.js:280:14)
-//     at REPLServer.runBound [as eval] (domain.js:293:12)
-//     at REPLServer.<anonymous> (repl.js:538:10)
-//     at emitOne (events.js:101:20)
-// > s = new Set(null)
-// Set {}
-// > s = new Set('')
-// Set {}
-// > s = new Set('thing')
-// Set { 't', 'h', 'i', 'n', 'g' }
-// ```
-//
-//
-// [11:00]
-// ```
-//
-//
-// [11:00]
-//  ```> s = new Set([[1,2],[3,4]])
-// Set { [ 1, 2 ], [ 3, 4 ] }```
-
-
-
-// test('test set', function (t) {
-//     var set = new NodeSet();
-//     t.equal(set.size, 0, 'set size is 0');
-//     set.add('raz').add('dwa').add({});
-//     t.equal(set.size, 3, 'set size is 3');
-//     set.add('foo');
-//     t.ok(set.has('raz'), 'set has raz');
-//     set.delete('dwa')
-//     t.notOk(set.has('dwa'), 'dwa deleted')
-//     t.notOk(set.has('andrea'), 'andrea was not here')
-//     var iterator = set.values();
-//     t.ok(iterator.next());
-//     t.end();
-// })
-//
-// test('test iterator', function(t) {
-//   var set = new Set();
-//   var iterator = set.values();
-//   t.end();
-// })
